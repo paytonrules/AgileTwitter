20e2f8cf58aa100d07f48eadf8fafa72afb7545d
Eric Smith <eric@8thlight.com>
2009-08-22 19:16:51 -0500

    Okay now I've got methods :)

--- Classes/ComposeTweetViewController.m
+++ Classes/ComposeTweetViewController.m
@@ -1,40 +1,7 @@
-//
-//  ComposeTweetViewController.m
-//  AgileTwitter
-//
-//  Created by Eric Smith on 8/22/09.
-//  Copyright 2009 8th Light. All rights reserved.
-//
-
 #import "ComposeTweetViewController.h"
 
 
 @implementation ComposeTweetViewController
-
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
@@ -52,6 +19,4 @@
 - (void)dealloc {
     [super dealloc];
 }
-
-
 @end
